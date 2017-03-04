81d693fe1fec758ca410c66e08ebe4291e27f991
Sergey Koshman <koshman.animys@gmail.com>
2017-03-04 16:32:59 +0200

    form study on learn.js - editable div and shortcut, but without reuse

--- formLearn/main.js
+++ formLearn/main.js
@@ -1,12 +1,11 @@
 'use strict';
 
-var form = document.forms;
-
-var select = form[0][0];
-
-var option = new Option('Классика', 'Classic', true, true);
-select.appendChild(option);
-
+// var form = document.forms;
+//
+// var select = form[0][0];
+//
+// var option = new Option('Классика', 'Classic', true, true);
+// select.appendChild(option);
 
 // solution of first task
 //
@@ -16,31 +15,91 @@ select.appendChild(option);
 //     console.log(select[i].value);
 //   }
 // }
+
+// solution of second task
+//
+// var container = document.getElementById('container');
+// var input = document.querySelector('input');
+//
+// input.addEventListener('focus', showPlaceholder);
+//
+// function showPlaceholder(e) {
+//   var tip = input.getAttribute('placeholder');
+//   var tipWrapper = document.createElement('div');
+//   tipWrapper.id = 'tip';
+//   tipWrapper.style.position = 'absolute';
+//   tipWrapper.style.top = '50px';
+//   tipWrapper.innerHTML = tip;
+//   tipWrapper.classList.add('for-placeholder');
+//   container.appendChild(tipWrapper);
+//
+//   input.placeholder = "";
+// }
+//
+// input.addEventListener('blur', hidePlaceholder);
+//
+// function hidePlaceholder() {
+//   var tip = document.getElementById('tip');
+//   tip.style.display = 'none';
+//
+//   if (input.value == "") {
+//     input.placeholder = "E-mail";
+//   }
+// }
+
+var div = document.querySelector('.field');
+
 var container = document.getElementById('container');
-var input = document.querySelector('input');
 
-input.addEventListener('focus', showPlaceholder);
+document.addEventListener('keydown', convertDiv);
 
-function showPlaceholder(e) {
-  var tip = input.getAttribute('placeholder');
-  var tipWrapper = document.createElement('div');
-  tipWrapper.id = 'tip';
-  tipWrapper.style.position = 'absolute';
-  tipWrapper.style.top = '50px';
-  tipWrapper.innerHTML = tip;
-  tipWrapper.classList.add('for-placeholder');
-  container.appendChild(tipWrapper);
+function convertDiv(e) {
+  // smth in right way to reuse, but not exactly
+  // if(!div) {
+  //   var div = document.querySelector('.field');
+  // }
 
-  input.placeholder = "";
-}
+  if(e.shiftKey && e.keyCode === 49) {
+    // console.log('begin edit');
+    container.removeChild(div);
 
-input.addEventListener('blur', hidePlaceholder);
+    var textarea = document.createElement('textarea');
+    textarea.classList.add('field');
+    textarea.setAttribute('autofocus', '');
+    container.appendChild(textarea);
 
-function hidePlaceholder() {
-  var tip = document.getElementById('tip');
-  tip.style.display = 'none';
+    var textarea = document.querySelector('textarea');
+    textarea.addEventListener('keydown', convertReverse);
+  }
 
-  if (input.value == "") {
-    input.placeholder = "E-mail";
+  function convertReverse(event){
+
+    if (event.shiftKey && event.keyCode === 83) {
+      // console.log('save and log out');
+      if (textarea.value) {
+        var content = textarea.value;
+        container.removeChild(textarea);
+
+        createEmptyDiv();
+
+      } else {
+        createEmptyDiv();
+      }
+
+    } else if(event.keyCode === 27) {
+      // console.log('log out without save');
+      container.removeChild(textarea);
+
+      createEmptyDiv();
+    }
+
+    function createEmptyDiv() {
+      var div = document.createElement('div');
+      div.innerHTML = content || "";
+      div.classList.add('field');
+      container.appendChild(div);
+    }
   }
 }
+
+// can't reuse second time - why?
