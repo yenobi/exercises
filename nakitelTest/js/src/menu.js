442c9a6146f68fd4a558e923f1ab28be3ab8e6f2
Sergey Koshman <koshman.animys@gmail.com>
2017-04-04 19:07:49 +0300

    nakitel - change location

--- nakitelTest/js/src/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var menuIcon = document.querySelector('.menu-icon');
-  var headerNav = document.querySelector('.header-nav');
-
-  menuIcon.addEventListener('click', function() {
-    headerNav.classList.toggle('shown');
-  });
-});
