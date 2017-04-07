070cf1551ebc4ae05e87ececff4c3b924fd4fff7
Sergey Koshman <koshman.animys@gmail.com>
2017-04-07 16:50:41 +0300

    start doing test-maker with oop and js

---
+++ makeTest/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function(document, window, domIsReady, undefined) {
+  domIsReady(function() {
+
+        let qNumber = document.getElementById('qNumber');
+        let question = document.getElementById('question');
+
+        qNumber.addEventListener('blur', function(){ 
+                // cycle of creating input-field for answers with i = this.value
+        });
+
+        question.addEventListener('blur', function(e) {
+                var question = this.value;
+        });
+
+
+        // cycle of creating input-text-field and input-checkbox fo 
+        // answers wrapping in function
+
+
+
+        // all oop-things will be after i can take all the data from page 
+
+
+
+
+  });
+})(document, window, domIsReady);
