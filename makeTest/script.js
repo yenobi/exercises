aaebffc70df2dd612b00085400088a8b07dcadbc
Sergey Koshman <koshman.animys@gmail.com>
2017-04-09 22:08:05 +0300

    makeTest - finish task with input-clear and button to clear localStorage

--- makeTest/script.js
+++ makeTest/script.js
@@ -1,16 +1,15 @@
 'use strict';
 
 (function(document, window, domIsReady, undefined) {
-  domIsReady(function() {
+  domIsReady(function(){
 
         let qNumber = document.getElementById('qNumber');
         let question = document.getElementById('question');
-        let form = document.querySelector('form');
         let answerWrapper = document.querySelector('.answer-wrapper');
         let button = document.querySelector('button');
 
         //  what kind of event will be better? tried with blur, changed
-        qNumber.addEventListener('blur', function(){ 
+        qNumber.addEventListener('blur', function(){
                 createAnswerInput(this.value);
         });
 
@@ -39,31 +38,32 @@
         }
 
         // try to create class name Test
-        function Test(question, answers, answerRight, key) {
-                this.question = question; 
-                this.answers = answers;
-                this.answerRight = answerRight;
-                this.key = key;
+        function Test(options) {
+                this.question = options.question; 
+                this.answers = options.answers;
+                this.answerRight = options.answerRight;
+                this.key = options.key;
         };
 
+
         // try to add some methods to class 
-        Test.prototype.print = function() {
+        Test.prototype.print = function() {         
                console.log(this);
         };
 
         Test.prototype.save = function() {
-                var json_test = JSON.stringify(this);
-                localStorage.setItem(this.key, json_test);
+                let jsonTest = JSON.stringify(this);
+                localStorage.setItem(this.key, jsonTest);
         };
 
         button.addEventListener('click', saveQuestion);
 
         function saveQuestion() {
-                var answers = Array.prototype.slice.call(document.querySelectorAll('.answer'));
-                var answersRadio = Array.prototype.slice.call(document.querySelectorAll('.answerRadio')); 
+                let answers = Array.prototype.slice.call(document.querySelectorAll('.answer'));
+                let answersRadio = Array.prototype.slice.call(document.querySelectorAll('.answerRadio')); 
 
                 // can do without external var?  x2
-                var answerRight;
+                let answerRight;
                 answersRadio.forEach(function(e) {
                         if (e.checked == true) {
                                 console.log(answersRadio.indexOf(e));
@@ -71,28 +71,37 @@
                         }
                 }, this);
 
-                const myTest = new Test(text, answers, answerRight, 'firstQuestion');
+                let parametrs = {
+                        question: text,
+                        answers: answers,
+                        answerRight: answerRight,
+                        key: 'firstQuestion'
+                };
+
+                let myTest = new Test(parametrs);
 
                 //  next 2 lines is for inheritance, but didn't wotj as i want 
                 myTest.prototype = Object.create(Test.prototype);
                 myTest.prototype.constructor = Test;
                 // this is for saving to localStorage with inner method of class 
                 myTest.save();
+
+                let allInputs = document.querySelectorAll('input');
+                allInputs.forEach(function(e){
+                        e.value = '';
+                });
         };
 
-        var forPrint = document.getElementById('forPrint');
+        let forPrint = document.getElementById('forPrint');
                 forPrint.addEventListener('click', function() {
-
-                        // search for my question 
-                        let printQ = JSON.parse(localStorage.getItem('firstQuestion'));
-                        
+  
+                        let printQ = new Test(JSON.parse(localStorage.getItem('firstQuestion')));      
                         printQ.print();
-
-                        // didn't see this - beacuse of scopes? 
-                        myTest.print();
-
-                        localStorage.clear();
                 });
-        
+
+        let cleaner = document.getElementById('cleaner');
+        cleaner.addEventListener('click', function() {
+                localStorage.clear();
+        });        
   });
 })(document, window, domIsReady);
