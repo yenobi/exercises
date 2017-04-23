715220555107fa09a13eb43918ab72b2565eaa95
Sergey Koshman <koshman.animys@gmail.com>
2017-04-23 16:24:42 +0300

    pingbuller - all js works awfull, have too many troubles

--- ping/src/js/menu.js
+++ ping/src/js/menu.js
@@ -7,7 +7,16 @@
 
         $('a.active').click( function() {
                 $('.header-nav').toggleClass('header-popup');
+                // не получается сделать анимацию для появления меню
+                // $('.header-nav-popup').animate({
+                //         opacity: 0.9
+                // }, 1000);
+
+                // вторая попытка была такая 
+                // $('.header-nav-popup').fadeIn('slow');
+                // тоже не работает
           } );
+
 
 })(window, document, undefined);
 
--- ping/src/js/topbutton.js
+++ ping/src/js/topbutton.js
@@ -11,8 +11,16 @@
                         $('.to-top').removeClass('to-top-show');
                 };
 
-                $('.to-top').click(function(){
-                       $('body').scrollTop(0);
+                $('.to-top-show').click(function(){
+                        // вот эта строка не блокирует скролл при возврате, 
+                        // но вверх не плавный скролл 
+                //        $('body').scrollTop(0);
+
+                // не могу понять, в чем здесь может быть ошибка 
+                        $('body').animate({
+                                scrollTop: 0
+                        }, 1000);
+
                 });
         });
 })(window, document, undefined);
