20d4fe5a00aaa64968146f84ae138e77949eef2f
Sergey Koshman <koshman.animys@gmail.com>
2017-04-25 21:50:25 +0300

    sedona - add page-header and welcome-screen for 320px

--- sedona/gulpfile.js
+++ sedona/gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('clean', function() {
 });
 // вотчер (пока только для стилей)
 gulp.task('watch', function() {
-  gulp.watch('src/scss/*.scss', gulp.series('sass'));
+  gulp.watch('src/scss/**/*', gulp.series('sass'));
 });
 
 // smth wrong with this task
