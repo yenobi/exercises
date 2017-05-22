8f343ef172894bc733c3d5d135220d82ba61f2d9
Sergey Koshman <koshman.animys@gmail.com>
2017-05-22 12:44:56 +0300

    sedona - fix centralizing in some blocks

---
+++ sedona/src/js/test11.js
@@ -0,0 +1,1 @@
+var a,w=document.createTreeWalker(document,NodeFilter.SHOW_TEXT);while(a=w.nextNode()){if(a.textContent.trim().length)a.textContent='Одиннадцатиклассница пошла посмотреть на достопримечательность, она шла долго, несколько строчек, пока не пришла'}
