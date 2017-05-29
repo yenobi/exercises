da0d609475b5dd17b22d7c20d30ebf83d5655830
Sergey Koshman <koshman.animys@gmail.com>
2017-05-29 14:54:58 +0300

    beside - start project

---
+++ beside/gulpfile.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const eslint = require('gulp-eslint');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const imagemin     = require('gulp-imagemin');
+const pngquant     = require('imagemin-pngquant');
+const cache = require('gulp-cache');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**.js')
+    .pipe(sourcemaps.init())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(concat('build.js'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('babel', function() {
+    return gulp.src('src/js/**.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('src/tmp'));
+});
+
+gulp.task('watch-js', function() {
+  gulp.watch('src/js/*', gulp.series('babel'));
+});
+
+gulp.task('img', function() {
+	return gulp.src('img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('clean', function() {
+  return del('build');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/scss/**/*', gulp.series('sass'));
+});
+
+gulp.task('build', gulp.series('clean', 'sass', 'js', 'img'));
+
+gulp.task('dev', gulp.series('build', 'watch'));
---
+++ builder/src/tmp/cache-exp.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function (window, document, undefined) {
+  'use strict';
+
+  console.log('ready');
+})(window, document, undefined);
