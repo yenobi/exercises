4ade844c3775b089e9fd07cb50df8254678fad95
Sergey Koshman <koshman.animys@gmail.com>
2017-06-14 09:37:19 +0300

    clean repo

--- webpack-study/home.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let welcome = require('./welcome');
-
-welcome('home');
-
-
-exports.welcome = welcome;
--- webpack-study/welcome.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(msg) {
-    alert(`Welcome ${msg}`);
-    
-    if(NODE_ENV == 'dev') {
-        console.log(msg);
-    }
-};
