c3d16b3b853a3e8e874ec61d00594254e92cf22c
Sergey Koshman <koshman.animys@gmail.com>
2017-07-27 22:31:02 +0300

    add eslint with airbnb-config, check and correct bookmarks-repo for mistakes with eslint

--- angular1.x/booky/app/categories/bookmarks/create/bookmark-create.js
+++ angular1.x/booky/app/categories/bookmarks/create/bookmark-create.js
@@ -1,47 +1,43 @@
-angular.module('categories.bookmarks.create', [
-
-])
-    .config(function($stateProvider) {
+angular.module('categories.bookmarks.create', [])
+    .config(($stateProvider) => {
         $stateProvider
             .state('booky.categories.bookmarks.create', {
                 url: '/bookmarks/create',
-                templateUrl: 'app/categories/bookmarks/create/bookmark-create.tmpl.html',
-                controller: 'CreateBookmarkCtrl as createBookmarkCtrl'
-            })
+                templateUrl:
+                    'app/categories/bookmarks/create/bookmark-create.tmpl.html',
+                controller: 'CreateBookmarkCtrl as createBookmarkCtrl',
+            });
     })
-    .controller('CreateBookmarkCtrl', function($state, $stateParams, BookmarksModel) {
-        var createBookmarkCtrl = this;
+    .controller('CreateBookmarkCtrl',
+        function CreateBookmarkCtrl($state, $stateParams, BookmarksModel) {
+            const createBookmarkCtrl = this;
 
-        function returnToBookmarks() {
-            $state.go('booky.categories.bookmarks', {
-                category: $stateParams.category
-            })
-        }
-
-        function cancelCreating() {
-            returnToBookmarks();
-        }
-
-        createBookmarkCtrl.cancelCreating = cancelCreating;
-
-        function createBookmark(bookmark) {
-            BookmarksModel.createBookmark(bookmark);
-            returnToBookmarks();
-        }
-
-        createBookmarkCtrl.createBookmark = createBookmark;
-
-        function resetForm() {
-            createBookmarkCtrl.newBookmark = {
-                titel: '',
-                url: '',
-                category: $stateParams.category
+            function returnToBookmarks() {
+                $state.go('booky.categories.bookmarks', {
+                    category: $stateParams.category,
+                });
             }
-        }
 
-        resetForm();
+            function cancelCreating() {
+                returnToBookmarks();
+            }
 
-        createBookmarkCtrl.tmpFunc = function() {
-            console.log(createBookmarkCtrl.newBookmark);
-        }
-    });
+            createBookmarkCtrl.cancelCreating = cancelCreating;
+
+            function createBookmark(bookmark) {
+                BookmarksModel.createBookmark(bookmark);
+                returnToBookmarks();
+            }
+
+            createBookmarkCtrl.createBookmark = createBookmark;
+
+            function resetForm() {
+                createBookmarkCtrl.newBookmark = {
+                    titel: '',
+                    url: '',
+                    category: $stateParams.category,
+                };
+            }
+
+            resetForm();
+        });
