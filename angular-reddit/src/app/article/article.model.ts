f8372e19abbebf832adb1862281928d749cdeed9
Sergey Koshman <koshman.animys@gmail.com>
2017-09-05 23:48:02 +0300

    add article-model, add articles-array, add possibility to add new articles, add sorting, finish sample of reddit-clone, deploy to now

---
+++ angular-reddit/src/app/article/article.model.ts
@@ -0,0 +1,29 @@
+export class Article {
+    votes: number;
+    title: string;
+    link: string;
+
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    voteUp(): void {
+        this.votes += 1;
+    }
+
+    voteDown(): void {
+        this.votes -= 1;
+    }
+
+    // domain() is a utility function that extracts the domain from a URL
+    domain(): string {
+        try {
+            const domainAndPath: string = this.link.split('//')[1];
+            return domainAndPath.split('/')[0];
+        } catch (err) {
+            return null;
+        }
+    }
+}
