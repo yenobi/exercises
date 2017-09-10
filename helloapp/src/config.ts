3b03f420ba1dee5a2c484e5479f741900bc79064
Sergey Koshman <koshman.animys@gmail.com>
2017-09-10 22:16:48 +0300

    housecleaning of hello-apps, init some-project (pluralsight-course), add event-list component

--- helloapp/src/config.ts
+++
@@ -1,3 +0,0 @@
-import { environment } from './environments/environment';
-
-export const DOMAIN: string = environment.domain;
---
+++ some/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { EventListComponent } from './event-list/event-list.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
