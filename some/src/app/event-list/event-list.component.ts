057f3908cbcace1601ab576871876b3a6c2c0ccb
Sergey Koshman <koshman.animys@gmail.com>
2017-09-27 18:59:44 +0300

    add routing to app, add mockup for user-profile page, add activated and dictivated routes, add 404-page for events'id that didn't find, add nested routes for user-profile-module

--- some/src/app/event-list/event-list.component.ts
+++ some/src/app/event-list/event-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 // routes to components - didn;t line them
-import { EventStorageService } from '../shared/event-storage.service';
-import { ToastrService } from '../common/toastr.service';
-
+import { EventStorageService } from '../shared/event-storage.service'
+import { ToastrService } from '../common/toastr.service'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   // selector: 'app-event-list',
@@ -10,17 +10,22 @@ import { ToastrService } from '../common/toastr.service';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
-  events: object[];
+  events: any;
 
   constructor(private eventStorageService: EventStorageService,
-                        private toastrService: ToastrService) {
+                        private toastrService: ToastrService,
+                        private route: ActivatedRoute) {
    }
 
   //  better to put such things (fetching data with ajax for exmaple)
   // to onInit part of components lifecycle, not in constructor - why ? 
   // it needs some time to execute -> longer loading ? 
   ngOnInit() {
-    this.events = this.eventStorageService.getEvents();
+    this.events = this.route.snapshot.data['events']
+    // this.eventStorageService.getEvents().subscribe(res => {
+    //   this.events = res;
+    // }
+    // );
   }
 
   handleThumbnailClick(eventName) {
---
+++ some/src/app/user/user.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { ProfileComponent } from './profile/profile.component'
+import { userRoutes } from './user.routes'
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(userRoutes)
+  ],
+  declarations: [
+    ProfileComponent,
+  ]
+})
+export class UserModule { }
