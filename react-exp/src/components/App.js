66926ec6b8ab212e49dfdd4af4d05de3280309b3
yenobi <koshman.animys@gmail.com>
2018-01-30 13:00:14 +0200

    add bootstrap-lib for experiments with styling react-components, add separeted folder for articleList (architectural experiment), add maping article-data to react-component for rendering with id-keys

--- react-exp/src/components/App.js
+++ react-exp/src/components/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Article from './Article';
 import articles from '../fixtures';
+import ArticleList from './ArticleList';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 function App() {
     return (
-      <div>
-          <h1>App name</h1>
-          <Article article={articles[0]} />
-          <Article article={articles[1]} />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="title">App name</h1>
+        </div>
+          <ArticleList articles={articles} />
       </div>
     );
 }
