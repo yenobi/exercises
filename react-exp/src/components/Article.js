93a153f7fd0f7321b0a9607e521c736eb67bcd24
yenobi <koshman.animys@gmail.com>
2018-02-11 20:52:50 +0200

    upd article component with comments inside + upd some markup for html5 tags

--- react-exp/src/components/Article.js
+++ react-exp/src/components/Article.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CommentList from './CommentList';
 
 // add PureComponent
 class Article extends React.PureComponent {
@@ -6,21 +7,25 @@ class Article extends React.PureComponent {
         const {article, isOpen, onButtonClick} = this.props;
         const body = isOpen ? <section style={{marginTop: '10px'}}>{article.text}</section> : '';
         const styleDate = {float: 'right'};
+        const comments = isOpen ? <CommentList comments={article.comments}/> : '';
         return (
-            <div className="card">
-                <div className="card-header">
+            <section className="card">
+                <header className="card-header">
                     <h2>
                         {article.title}
                         <button className="btn btn-primary float-right" onClick={onButtonClick}>
                             {isOpen ? 'close' : 'open'}
                         </button>    
                     </h2>
-                </div>
+                </header>
                 <div className="card-body">
                     <h6 className="card-subtitle text-muted" style={styleDate}>date: {(new Date(article.date)).toDateString()}</h6>
                     {body}
                 </div>
-            </div>
+                <footer>
+                    {comments}
+                </footer>
+            </section>
         );
     }
 }
