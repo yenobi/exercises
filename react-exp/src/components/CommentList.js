16091d846a1ab95630e7d8e87c8e9f9e3a62c665
yenobi <koshman.animys@gmail.com>
2018-02-17 20:34:35 +0200

    refactor commet list from class to functional component

--- react-exp/src/components/CommentList.js
+++ react-exp/src/components/CommentList.js
@@ -2,37 +2,32 @@ import React from 'react';
 import Comment from './Comment';
 import toggleOpen from '../decorators/toggleOpen';
 
-class CommentList extends React.Component {
-    static defaultProps = {
-        comments: []
-    }
-
-    renderList = () => {
-        if (!this.props.isOpen) return null;
-
-        if(!this.props.comments) return <p>No comments yet</p>
-
-        return (
-                <ul>
-                    {Object.values(this.props.comments)
-                        .map(comment => {
-                            return <li key={comment.id}><Comment comment={comment} /></li>
-                        })}
-                </ul> 
-        );
-    }
-
-    render() {
+function CommentList({isOpen, comments = [], toggleOpen}) {
         return (
             <div>
-                <button type="button" className="btn btn-primary" onClick={this.props.toggleOpen}>
-                    {this.props.isOpen ? 'close' : 'open'} comments
+                <button type="button" className="btn btn-primary" onClick={toggleOpen}>
+                    {isOpen ? 'close' : 'open'} comments
                 </button>
-                {this.renderList()}
+                {/* {this.renderList()} */}
+                {renderList(isOpen, comments)}
             </div>
            
         );
-    }
+}
+
+function renderList(isOpen, comments) {
+    if (!isOpen) return null;
+
+    if(!comments) return <p>No comments yet</p>
+
+    return (
+            <ul>
+                {Object.values(comments)
+                    .map(comment => {
+                        return <li key={comment.id}><Comment comment={comment} /></li>
+                    })}
+            </ul> 
+        );
 }
 
 export default toggleOpen(CommentList);
